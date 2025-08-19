0907bc8dfefa7dd52da1a3d9cc7a0d5e2eb3148a
B <beversprof25@outlook.com>
2025-08-20 00:56:19 +0100

    Add unit test for home endpoint(data and status code)

---
+++ day1-ip-api/tests/test_main.py
@@ -0,0 +1,13 @@
+from src.main import app
+
+import pytest
+
+def test_home_endpoint():
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.data.decode("utf-8") == "<h1>Hello World</h1>"
+        assert response.status_code == 200
+
+
+def test_greet_endpoint():
+    pass
