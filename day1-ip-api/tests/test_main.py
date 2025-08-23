3d6f9eb58183c6dca6e2d7a3ef816048c552a6f5
B <beversprof25@outlook.com>
2025-08-24 00:05:45 +0100

    Add comment for the greet endpoint ('/greet/<name>')

--- day1-ip-api/tests/test_main.py
+++ day1-ip-api/tests/test_main.py
@@ -7,7 +7,7 @@ def test_home_endpoint():
         assert response.data.decode("utf-8") == "<h1>Hello World</h1>"
         assert response.status_code == 200
 
-
+# Test the greet endpoint ("/greet/<name>")
 def test_greet_endpoint():
     with app.test_client() as client:
         response = client.get("/greet/John")
