21b2b6924b905164c7b192e2088ed5ef5ec9d355
B <beversprof25@outlook.com>
2025-09-12 00:37:07 +0100

    Add basic logging for incoming requests on / and /greet/<name>

--- day1-ip-api/src/main.py
+++ day1-ip-api/src/main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+import logging
+
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 @app.route("/")
@@ -11,6 +18,10 @@ def home():
     Returns:
         str: An HTML string containing "Hello World".
     """
+    logging.info(
+        f"Received {request.method} at {request.path} from {request.remote_addr}"
+    )
+
     return "<h1>Hello World</h1>"
 
 
@@ -25,8 +36,11 @@ def greet(name):
     Returns:
         str: A greeting message that includes the given name.
     """
+    logging.info(f"Received {request.method} at {name} from {request.remote_addr}")
+
     return f"Hello {name}!"
 
 
 if __name__ == "__main__":
+    print("Started")
     app.run("0.0.0.0", port=5000, debug=True)
