de644e04acfb5405cf951aa4b800f0192ade2179
B <beversprof25@outlook.com>
2025-09-13 22:10:24 +0100

    feat: added tr/except with logging for '/' and 'greet' endpoints so errors dont crash the app

--- day1-ip-api/src/main.py
+++ day1-ip-api/src/main.py
@@ -12,73 +12,83 @@ logging.basicConfig(
 
 @app.route("/")
 def home():
-    """
-    Homepage endpoint that displays a simple greeting message.
+    try:
+        # x = 1/0  # Used to test endpoint-wide error handling, comment out in production
+        """
+        Homepage endpoint that displays a simple greeting message.
 
-    Returns:
+        Returns:
         str: An HTML string containing "Hello World".
-    """
-    lang_str = request.args.get("lang", "0")
-    try:
-        lang = int(lang_str)
-    except ValueError as e:
-        logging.exception("Invalid parameter")
-        lang = 0
+        """
+        lang_str = request.args.get("lang", "0")
+        try:
+            lang = int(lang_str)
+        except ValueError as e:
+            logging.exception("Invalid parameter")
+            lang = 0
 
-    agent = request.headers.get("User-Agent")
-    try:
-        agent = agent.upper()
-    except AttributeError as e:
-        logging.exception("Invalid method used, value is not a string")
-        agent = ""
+        agent = request.headers.get("User-Agent")
+        try:
+            agent = agent.upper()
+        except AttributeError as e:
+            logging.exception("Invalid method used, value is not a string")
+            agent = ""
 
-    logging.info(
-        f"Received {request.method} at {request.path} from {request.remote_addr} "
-        f"query param is {lang} and client is {agent}"
-    )
+        logging.info(
+            f"Received {request.method} at {request.path} from {request.remote_addr} "
+            f"query param is {lang} and client is {agent}"
+        )
 
-    status_code = 200
-    response_body = "<h1>Hello World</h1>"
-    logging.info(f"Response sent | status: {status_code} | body: {response_body}")
+        status_code = 200
+        response_body = "<h1>Hello World</h1>"
+        logging.info(f"Response sent | status: {status_code} | body: {response_body}")
 
-    return "<h1>Hello World</h1>"
+        return "<h1>Hello World</h1>"
+    except Exception as e:
+        logging.exception("Unexpected error")
+        return "<h1>Unexpected error</h1>", 500
 
 
 @app.route("/greet/<name>")
 def greet(name):
-    """
-    Greeting endpoint that personalizes the response with the given name.
-
-    Args:
-        name (str): The name provided in the URL path.
-
-    Returns:
-        str: A greeting message that includes the given name.
-    """
-    lang_str = request.args.get("lang", "0")
     try:
-        lang = int(lang_str)
-    except ValueError as e:
-        logging.exception("Invalid parameter")
-        lang = 0
+        # x = 1/0  # Used to test endpoint-wide error handling, comment out in production
+        """
+        Greeting endpoint that personalizes the response with the given name.
 
-    agent = request.headers.get("User-Agent")
-    try:
-        agent = agent.upper()
-    except AttributeError as e:
-        logging.exception("Invalid method used, value is not a string")
-        agent = ""
+        Args:
+            name (str): The name provided in the URL path.
 
-    logging.info(
-        f"Received {request.method} at {name} from {request.remote_addr} "
-        f"query param is {lang} and client is {agent}"
-    )
+        Returns:
+            str: A greeting message that includes the given name.
+        """
+        lang_str = request.args.get("lang", "0")
+        try:
+            lang = int(lang_str)
+        except ValueError as e:
+            logging.exception("Invalid parameter")
+            lang = 0
 
-    status_code = 200
-    response_body = f"Hello {name}!"
-    logging.info(f"Response sent | status: {status_code} | body: {response_body}")
+        agent = request.headers.get("User-Agent")
+        try:
+            agent = agent.upper()
+        except AttributeError as e:
+            logging.exception("Invalid method used, value is not a string")
+            agent = ""
 
-    return f"Hello {name}!"
+        logging.info(
+            f"Received {request.method} at {name} from {request.remote_addr} "
+            f"query param is {lang} and client is {agent}"
+        )
+
+        status_code = 200
+        response_body = f"Hello {name}!"
+        logging.info(f"Response sent | status: {status_code} | body: {response_body}")
+
+        return f"Hello {name}!"
+    except Exception as e:
+        logging.exception("Unexpected error")
+        return "<h1>Unexpected error</h1>", 500
 
 
 if __name__ == "__main__":
