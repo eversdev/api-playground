ee1adb8d95a2f15f35eb84176fda1c1f2a81b137
B <beversprof25@outlook.com>
2025-09-13 23:58:09 +0100

    chore: start implementing endpoint-wide error handling test

--- day1-ip-api/tests/test_main.py
+++ day1-ip-api/tests/test_main.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from src.main import app
+
 
 # Test the home endpoint ("/")
 def test_home_endpoint():
@@ -7,9 +10,19 @@ def test_home_endpoint():
         assert response.data.decode("utf-8") == "<h1>Hello World</h1>"
         assert response.status_code == 200
 
+
 # Test the greet endpoint ("/greet/<name>")
 def test_greet_endpoint():
     with app.test_client() as client:
         response = client.get("/greet/John")
         assert response.data.decode("utf-8") == "Hello John!"
         assert response.status_code == 200
+
+
+@patch("src.main.home.int")
+def test_home_endpoint_failure():
+    pass
+
+
+def test_greet_endpoint_failure():
+    pass
