798b203e5393c25dc9ca34728075eaa2abe7b1c6
B <beversprof25@outlook.com>
2025-09-15 16:22:55 +0100

    Day 1 - added request logging to / endpoint

--- FastAPI_version/day1-ip-api/src/main.py
+++ FastAPI_version/day1-ip-api/src/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+import logging
+
 import uvicorn
+from fastapi import FastAPI, Request
+
+logger = logging.getLogger("uvicorn")
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def home():
+def home(request: Request):
+
+    method = request.method
+    url = request.url.path
+
+    logger.info(f"The {method} method was called and the url path was {url}")
+
     return "Hello world"
