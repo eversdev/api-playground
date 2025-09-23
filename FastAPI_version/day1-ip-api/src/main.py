ef2aa9c59165052bf3e23aa22f04bd1eee85ccca
B <beversprof25@outlook.com>
2025-09-23 06:50:16 +0100

    Set up pipenv environment and added /hello{name} and /sum endpoints

--- FastAPI_version/day1-ip-api/src/main.py
+++ FastAPI_version/day1-ip-api/src/main.py
@@ -1,4 +1,6 @@
 import logging
+#import sys
+#print(sys.executable)
 
 import uvicorn
 from fastapi import FastAPI, Request
@@ -18,3 +20,13 @@ def home(request: Request):
     logger.info(f"The {method} method was called and the url path was {url}")
 
     return {"message": "Hello world"}
+
+
+
+@app.get("/hello/{name}")
+def greeting(name: str):
+    return {"hello": name}
+
+@app.get("/sum")
+def calculate_sum(a:int, b:int):
+    return {"sum": a + b}
