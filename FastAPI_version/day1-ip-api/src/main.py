613eaa4cd572259ac69591ff22b3537a393703bb
B <beversprof25@outlook.com>
2025-09-27 05:13:46 +0100

    chore: restructure project,udpate dockerfile &docker compose for pgsql and fastapi

--- FastAPI_version/day1-ip-api/src/main.py
+++ FastAPI_version/day1-ip-api/src/main.py
@@ -1,6 +1,8 @@
 import logging
-#import sys
-#print(sys.executable)
+import psycopg2
+
+# import sys
+# print(sys.executable)
 
 import uvicorn
 from fastapi import FastAPI, Request
@@ -22,30 +24,37 @@ def home(request: Request):
     return {"message": "Hello world"}
 
 
-
 @app.get("/hello/{name}")
 def greeting(request: Request, name: str):
     method = request.method
     url = request.url.path
 
-
-
     logger.info(
-          f"The method used was {method}, the URL path was {url}, "
-          f"and the path parameter was {name}"
-          )
+        f"The method used was {method}, the URL path was {url}, "
+        f"and the path parameter was {name}"
+    )
 
     return {"hello": name}
 
+
 @app.get("/sum")
-def calculate_sum(request: Request, a:int, b:int):
+def calculate_sum(request: Request, a: int, b: int):
 
     method = request.method
 
     logger.info(
-        f"The method used is {method} and the "
-        f"query parameters are {a} and {b}"
+        f"The method used is {method} and the " f"query parameters are {a} and {b}"
     )
 
-
     return {"sum": a + b}
+
+
+db_connection = psycopg2.connect(
+    user="admin", password="password", dbname="postgres", host="postgres", port=5432
+)
+
+cur = db_connection.cursor()
+cur.execute("SELECT 1")
+result = cur.fetchone()
+logger.info(f"Postgres test result: {result}")
+cur.close()
